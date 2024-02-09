4f7007d026bda4218e5365d439babc7b6b7334f1
alan_omam <omamalan@hotmail.it>
2024-02-09 15:30:59 +0100

    impostato alcuni parametri per la data di partenza e la data di arrivo

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattin
  
  */
 
- let d = new Date()
- console.log(d)
- console.log( "hours",d.getHours())
- console.log( "minutes",d.getMinutes())
- console.log( "seconds",d.getSeconds())
+ let d1 = new Date()
+ console.log(d1)
+ console.log( "hours",d1.getHours())
+ console.log( "minutes",d1.getMinutes())
+ console.log( "seconds",d1.getSeconds())
+ console.log("milliseconds",d1.getMilliseconds())
+
+ let d2 = new Date("2024-02-12")
+console.log(d2)
+console.log(d2.setHours(9))
+console.log(d2.getHours())
+console.log(d2.setMinutes(30))
+console.log(d2.getMinutes())
+ 
+ 
