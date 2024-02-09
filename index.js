7773b2b75f1721577c11267b716964e06d8f7fda
alan_omam <omamalan@hotmail.it>
2024-02-09 15:06:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+ /*
+ consegna:
+Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
+ 
+ */
