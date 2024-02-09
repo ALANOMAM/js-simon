fd4f36ef0508c0d0105abbb57019e746c2f81697
alan_omam <omamalan@hotmail.it>
2024-02-09 15:49:48 +0100

    trovare la differenza di tempo e gorni tra due date a caso

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
  
  */
-
+/*
  let d1 = new Date()
  console.log(d1)
  console.log( "hours",d1.getHours())
@@ -18,5 +18,19 @@ console.log(d2.setHours(9))
 console.log(d2.getHours())
 console.log(d2.setMinutes(30))
 console.log(d2.getMinutes())
+ */
+
+let data1 = new Date("01/16/2024");
+let data2 = new Date("01/26/2024");
  
+// calcolo la differenza di tempo tra due date indicative
+let differenzaDiTempo =
+    data2.getTime() - data1.getTime();
+    console.log("differenza di tempo in millisecondi",differenzaDiTempo)
+ 
+//dalla differenza di tempo trovato sopra, ricavo la differenza di giorni.
+let differenzaDiGiorni = Math.round(differenzaDiTempo / (1000 * 3600 * 24));
+console.log("differenza di tempo di giorni",differenzaDiGiorni)
+ 
+
  
