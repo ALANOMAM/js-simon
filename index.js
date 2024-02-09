34dba792b16df933f7fd4f528e0ebaa81d46ca59
alan_omam <omamalan@hotmail.it>
2024-02-09 15:13:13 +0100

    impostato il metodo date

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
  
  */
+
+ let d = new Date()
+ console.log(d)
+ console.log( "hours",d.getHours())
+ console.log( "minutes",d.getMinutes())
+ console.log( "seconds",d.getSeconds())
